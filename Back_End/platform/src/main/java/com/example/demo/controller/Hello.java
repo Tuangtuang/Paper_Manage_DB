ac4f0bb5fd5624570e2a9c363935fbea3101039a
Tuangtuang <tangyuqi00@qq.com>
2019-03-27 20:22:06 +0800

    init
    后端初始化

---
+++ Back_End/platform/src/main/java/com/example/demo/controller/Hello.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Hello {
+    @RequestMapping("/hello")
+    public String index(){
+        return "Hello World";
+    }
+
+}
