d7ba5ae845b81e1456d00514bce8de2fb0f955b2
Tuangtuang <tangyuqi00@qq.com>
2019-05-22 14:42:51 +0800

    generator

---
+++ Back_End/platform/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@MapperScan("com.example.demo.dao")
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ Back_End/platform/src/main/java/com/example/demo/dao/QuestionMapper.java
@@ -0,0 +1,37 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.entity.Question;
+import com.example.demo.model.entity.QuestionExample;
+import com.example.demo.model.entity.QuestionWithBLOBs;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface QuestionMapper {
+    int countByExample(QuestionExample example);
+
+    int deleteByExample(QuestionExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(QuestionWithBLOBs record);
+
+    int insertSelective(QuestionWithBLOBs record);
+
+    List<QuestionWithBLOBs> selectByExampleWithBLOBs(QuestionExample example);
+
+    List<Question> selectByExample(QuestionExample example);
+
+    QuestionWithBLOBs selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") QuestionWithBLOBs record, @Param("example") QuestionExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") QuestionWithBLOBs record, @Param("example") QuestionExample example);
+
+    int updateByExample(@Param("record") Question record, @Param("example") QuestionExample example);
+
+    int updateByPrimaryKeySelective(QuestionWithBLOBs record);
+
+    int updateByPrimaryKeyWithBLOBs(QuestionWithBLOBs record);
+
+    int updateByPrimaryKey(Question record);
+}
